8c2b99bdb3bb1ab2354a5ff48df0a4aa1d870d2e
Rob Jinman <admin@robjinman.com>
2015-04-04 20:17:58 +0100

    Barebones Angular front-end added.

---
+++ front/README.md
@@ -0,0 +1,6 @@
+#Barebones AngularJS Application
+
+To get started, run
+    npm install
+
+If run as root npm downgrades its permissions to nobody to run scripts, which causes **bower install** to fail.
---
+++ front/app/js/dev/app.js
@@ -0,0 +1,33 @@
+/**
+* @module App
+* @requires ngRoute
+* @requires ngCookies
+* @requires jlUtil
+* @requires jlBackend
+* @requires jlLayout
+* @requires jlScroll
+*/
+var app = angular.module("App", [
+  "ui.bootstrap",
+  "ngRoute",
+  "ngCookies",
+  "jlUtil",
+  "jlScroll",
+  "jlDirectives"])
+
+  .config(["$logProvider", function($logProvider) {
+    $logProvider.debugEnabled(true);
+  }])
+
+  .config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+    $routeProvider.when("/index", {
+      templateUrl: "templates/views/index_vw.html",
+      controller: "IndexCtrl",
+      controllerAs: "ctrl"
+    })
+    .otherwise({
+      redirectTo: "/index"
+    });
+
+//    $locationProvider.html5Mode(true);
+  }]);
